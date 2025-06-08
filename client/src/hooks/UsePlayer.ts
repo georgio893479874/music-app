931d3f1ad3832742e47e49a21a78f36a63b3d9ea
George Al-Khames <georgijalhames@gmail.com>
2025-06-08 23:02:17 +0300

    :commit

--- client/src/hooks/UsePlayer.ts
+++ client/src/hooks/UsePlayer.ts
@@ -11,27 +11,41 @@ const usePlayer = ({
   const [isPlaying, setIsPlaying] = useState<boolean>(false);
   const [currentTime, setCurrentTime] = useState(0);
   const [duration, setDuration] = useState(0);
-  const audioPlayer = useRef<HTMLAudioElement>(new Audio());
+  const audioPlayer = useRef<HTMLAudioElement | null>(null);
   const progressBar = useRef<HTMLInputElement>(null);
   const { setSelectedSong } = usePlayerContext();
+
+  useEffect(() => {
+    if (typeof window !== "undefined" && audioPlayer.current === null) {
+      audioPlayer.current = new window.Audio();
+    }
+  }, []);
 
   useEffect(() => {
     const audio = audioPlayer.current;
 
     const handleTimeUpdate = () => {
-      setCurrentTime(audio.currentTime);
+      if (audio) {
+        setCurrentTime(audio.currentTime);
+      }
     };
 
     const handleLoadedMetadata = () => {
-      setDuration(audio.duration);
+      if (audio) {
+        setDuration(audio.duration);
+      }
     };
 
-    audio.addEventListener("timeupdate", handleTimeUpdate);
-    audio.addEventListener("loadedmetadata", handleLoadedMetadata);
+    if (audio) {
+      audio.addEventListener("timeupdate", handleTimeUpdate);
+      audio.addEventListener("loadedmetadata", handleLoadedMetadata);
+    }
 
     return () => {
-      audio.removeEventListener("timeupdate", handleTimeUpdate);
-      audio.removeEventListener("loadedmetadata", handleLoadedMetadata);
+      if (audio) {
+        audio.removeEventListener("timeupdate", handleTimeUpdate);
+        audio.removeEventListener("loadedmetadata", handleLoadedMetadata);
+      }
     };
   }, [audioPlayer]);
 
@@ -47,40 +61,54 @@ const usePlayer = ({
       });
 
       navigator.mediaSession.setActionHandler("play", () => {
-        audio.play();
-        setIsPlaying(true);
+        if (audio) {
+          audio.play();
+          setIsPlaying(true);
+        }
       });
 
       navigator.mediaSession.setActionHandler("pause", () => {
-        audio.pause();
+        if (audio) {
+          audio.pause();
+        }
         setIsPlaying(false);
       });
 
       navigator.mediaSession.setActionHandler("previoustrack", () => {
-        audio.currentTime = 0;
+        if (audio) {
+          audio.currentTime = 0;
+        }
         if (currentSongIndex > 0) {
           setCurrentSongIndex(currentSongIndex - 1);
         }
       });
 
       navigator.mediaSession.setActionHandler("nexttrack", () => {
-        audio.currentTime = 0;
+        if (audio) {
+          audio.currentTime = 0;
+        }
         if (currentSongIndex < songs.length - 1) {
           setCurrentSongIndex(currentSongIndex + 1);
         }
       });
 
       navigator.mediaSession.setActionHandler("seekbackward", () => {
-        audio.currentTime = Math.max(audio.currentTime - 10, 0);
+        if (audio) {
+          audio.currentTime = Math.max(audio.currentTime - 10, 0);
+        }
       });
 
       navigator.mediaSession.setActionHandler("seekforward", () => {
-        audio.currentTime = Math.min(audio.currentTime + 10, audio.duration);
+        if (audio) {
+          audio.currentTime = Math.min(audio.currentTime + 10, audio.duration);
+        }
       });
 
       navigator.mediaSession.setActionHandler("stop", () => {
-        audio.pause();
-        audio.currentTime = 0;
+        if (audio) {
+          audio.pause();
+          audio.currentTime = 0;
+        }
         setIsPlaying(false);
       });
     }
@@ -96,12 +124,14 @@ const usePlayer = ({
     const loadSong = async () => {
       if (songs.length > 0) {
         const song = songs[currentSongIndex];
-        const previousTime = audioPlayer.current.currentTime;
-        audioPlayer.current.src = song.audioFilePath;
-        audioPlayer.current.load();
-        audioPlayer.current.currentTime = previousTime;
-        if (isPlaying) {
-          audioPlayer.current.play();
+        if (audioPlayer.current) {
+          const previousTime = audioPlayer.current.currentTime;
+          audioPlayer.current.src = song.audioFilePath;
+          audioPlayer.current.load();
+          audioPlayer.current.currentTime = previousTime;
+          if (isPlaying) {
+            audioPlayer.current.play();
+          }
         }
       }
     };
@@ -111,43 +141,55 @@ const usePlayer = ({
   useEffect(() => {
     const audio = audioPlayer.current;
 
-    const handleEnded = () => {
-      if (repeatMode === "one") {
-        audio.currentTime = 0;
-        audio.play();
-      } else if (repeatMode === "all") {
-        if (currentSongIndex < songs.length - 1) {
-          setCurrentSongIndex(currentSongIndex + 1);
-          setSelectedSong(songs[currentSongIndex + 1]);
+    if (audio) {
+      const handleEnded = () => {
+        if (repeatMode === "one") {
+          audio.currentTime = 0;
+          audio.play();
+        } else if (repeatMode === "all") {
+          if (currentSongIndex < songs.length - 1) {
+            setCurrentSongIndex(currentSongIndex + 1);
+            setSelectedSong(songs[currentSongIndex + 1]);
+          } else {
+            setCurrentSongIndex(0);
+          }
+          audio.currentTime = 0;
         } else {
-          setCurrentSongIndex(0);
+          if (currentSongIndex < songs.length - 1) {
+            setCurrentSongIndex(currentSongIndex + 1);
+            setSelectedSong(songs[currentSongIndex + 1]);
+          } else {
+            setCurrentSongIndex(0);
+            setSelectedSong(songs[0]);
+          }
+          audio.currentTime = 0;
         }
-        audio.currentTime = 0;
-      } else {
-        if (currentSongIndex < songs.length - 1) {
-          setCurrentSongIndex(currentSongIndex + 1);
-          setSelectedSong(songs[currentSongIndex + 1]);
-        } else {
-          setCurrentSongIndex(0);
-          setSelectedSong(songs[0]);
-        }
-        audio.currentTime = 0;
-      }
-    };
+      };
 
-    audio.addEventListener("ended", handleEnded);
+      audio.addEventListener("ended", handleEnded);
 
-    return () => {
-      audio.removeEventListener("ended", handleEnded);
-    };
-  }, [currentSongIndex, setCurrentSongIndex, songs, repeatMode, setSelectedSong]);
+      return () => {
+        audio.removeEventListener("ended", handleEnded);
+      };
+    }
+  }, [
+    currentSongIndex,
+    setCurrentSongIndex,
+    songs,
+    repeatMode,
+    setSelectedSong,
+  ]);
 
   const togglePlayPause = () => {
     const audio = audioPlayer.current;
     if (isPlaying) {
-      audio.pause();
+      if (audio) {
+        audio.pause();
+      }
     } else {
-      audio.play();
+      if (audio) {
+        audio.play();
+      }
     }
     setIsPlaying(!isPlaying);
   };
@@ -157,7 +199,7 @@ const usePlayer = ({
     const seconds = Math.floor(time % 60);
     return `${minutes}:${seconds}`;
   };
-  
+
   const handleProgressChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (audioPlayer.current) {
       const newTime = Number(e.target.value);
@@ -175,7 +217,9 @@ const usePlayer = ({
       setCurrentSongIndex(prevIndex);
       setSelectedSong(songs[prevIndex]);
     }
-    audioPlayer.current.currentTime = 0;
+    if (audioPlayer.current) {
+      audioPlayer.current.currentTime = 0;
+    }
   };
 
   const skipEnd = () => {
@@ -184,7 +228,9 @@ const usePlayer = ({
       setCurrentSongIndex(nextIndex);
       setSelectedSong(songs[nextIndex]);
     }
-    audioPlayer.current.currentTime = 0;
+    if (audioPlayer.current) {
+      audioPlayer.current.currentTime = 0;
+    }
   };
 
   return {
