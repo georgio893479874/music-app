4096a672a872ea55e3ec1073145d67a0673c7bd8
georgio893479874 <svipe83@gmail.com>
2025-06-14 16:14:47 +0300

    commit #42

--- client/src/app/oauth/success/page.tsx
+++ client/src/app/oauth/success/page.tsx
@@ -1,11 +1,17 @@
 "use client";
 
-import { useEffect } from "react";
+import { Suspense, useEffect } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 
-export const dynamic = "force-dynamic";
-
 export default function OAuthSuccessPage() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <OAuthSuccessContent />
+    </Suspense>
+  );
+}
+
+function OAuthSuccessContent() {
   const router = useRouter();
   const searchParams = useSearchParams();
   const token = searchParams.get("token");
